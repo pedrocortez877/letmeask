6b9ec50491af37f873f5edda74ec9598fb1bcaf0
Pedro Henrique Ribeiro Cortez <pedrocortez877@gmail.com>
2021-06-25 22:45:19 -0300

    Dia 04 - criação e leitura de mensagens e criação do hook useRoom

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,18 +1,15 @@
 import { useHistory } from 'react-router';
 import { FormEvent, useState } from 'react';
 
-import { useAuth } from '../hooks/useAuth';
-import {Button} from '../components/Button';
-import { database } from '../services/firebase';
+import { useAuth } from '../../hooks/useAuth';
+import { Button } from '../../components/Button';
+import { database } from '../../services/firebase';
 
-import illustrationImg from '../assets/images/illustration.svg';
-import logoImg from '../assets/images/logo.svg';
-import googleIconImg from '../assets/images/google-icon.svg';
+import illustrationImg from '../../assets/images/illustration.svg';
+import logoImg from '../../assets/images/logo.svg';
+import googleIconImg from '../../assets/images/google-icon.svg';
 
-import '../styles/auth.scss';
-
-
-
+import './styles.scss';
 
 export function Home(){
   const history = useHistory();
@@ -38,6 +35,11 @@ export function Home(){
 
     if(!roomRef.exists()){
       alert('Room does not exists');
+    }
+
+    if(roomRef.val().endedAt){
+      alert('Room already closed');
+      return;
     }
     
     history.push(`/rooms/${roomCode}`);
